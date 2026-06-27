36f072598c30479f951a2589f23ed7fc1154abc5
Lucare76 <luca_renna@hotmail.com>
2026-06-27 14:33:32 +0200

    Improve footer link accessibility

--- components/site/ManageCookiesButton.tsx
+++ components/site/ManageCookiesButton.tsx
@@ -5,16 +5,21 @@ export function ManageCookiesButton({ locale }: { locale: Locale }) {
   return (
     <button
       type="button"
+      className="footer-manage-cookies"
       onClick={() => window.dispatchEvent(new Event("im_manage_cookies"))}
       style={{
         background: "none",
         border: "none",
         cursor: "pointer",
-        padding: 0,
+        padding: "10px 12px",
+        minHeight: 44,
+        minWidth: 44,
         font: "inherit",
         color: "inherit",
         textDecoration: "underline",
-        display: "inline",
+        display: "inline-flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
       {locale === "it" ? "Gestisci cookie" : "Manage cookies"}
